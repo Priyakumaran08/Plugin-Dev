23b5ea7e8079e3791c251accb254e2267f35fe1a
Priyakumaran08 <priyakumaran181@gmail.com>
2025-03-19 17:28:57 +0530

    Added Plugin detail info

---
+++ wp-content/plugins/devy-gallery-plugin/devy-gallery-plugin.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Devy Gallery Plugin
+ * Plugin URI:  https://devymuse.com/devy-galler-plugin
+ * Author:      Devy Muse
+ * Author URI:  Htps://devymuse.com
+ * Description: This is a Gallery plugin developed by Devy Muse.
+ * Version:     0.1.0
+ * License:     GPL-2.0+
+ * License URL: http://www.gnu.org/licenses/gpl-2.0.txt
+ * text-domain: devy-galler-plugin
+*/
