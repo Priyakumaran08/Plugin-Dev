05c843e540331789fc097178d13adf0b95cc5562
Priyakumaran08 <priyakumaran181@gmail.com>
2025-03-21 18:07:14 +0530

    Added Labels code

--- wp-content/plugins/devy-gallery-plugin/includes/class-devy-gallery-plugin.php
+++ wp-content/plugins/devy-gallery-plugin/includes/class-devy-gallery-plugin.php
@@ -16,9 +16,29 @@ class Devy_Gallery_Plugin{
     }
 
     public function registerPostType(){
+
+        //labels
+        $labels = [
+            'name'               => 'Devy Gallery',
+            'singular_name'      => 'Devy Gallery',
+            'menu_name'          => 'Devy Gallery',
+            'name_admin_bar'     => 'Devy Gallery',
+            'add_new'            => 'Add New',
+            'add_new_item'       => 'Add New Devy Gallery',
+            'new_item'           => 'New Devy Gallery',
+            'edit_item'          => 'Edit Devy Gallery',
+            'view_item'          => 'View Devy Gallery',
+            'all_items'          => 'All Devy Galleries',
+            'search_items'       => 'Search Devy Galleries',
+            'parent_item_colon'  => 'Parent Devy Gallery:',
+            'not_found'          => 'No Devy Gallery found.',
+            'not_found_in_trash' => 'No Devy Gallery found in Trash.',
+        ];
+        
         //args
         $args = [
             'label'=>"Devy Gallery Plugin",
+            'labels' => $labels,
             'description' => "Devy Gallery Plugin for Plugindevs",
             'show_ui' => true,
         ];
